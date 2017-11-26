4e39b333cd2fbdfb131f53bf9f2646cc1f1f5467
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-26 06:04:32 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,40 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all
   @@songs = 0
 
     def initialize(name)
       @name = name
+      @songs = []
     end
 
-    def songs
-      @@songs = songs
+    def self.all
+      @@all
+    end
+
+    def add_song(song)
+      @songs << songs
+    end
+
+    def self.find_or_create_by_name(name)
+      self.find(name) ? self.find(name) : self.create(name)
+    end
+
+    def self.find(name)
+      self.all.find {|artist| artist.name == name }
+    end
+
+    def self.create(name)
+      self.new(name).tap {|artist| artist.save }
+    end
+
+    def save
+      @@all << self
+    end
+
+    def print_songs
+      songs.each {|song| puts song.name }
     end
 
 end
